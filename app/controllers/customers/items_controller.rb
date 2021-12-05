4319176ba35f5563e57eb0bfe760771f1698039a
Takihamu <yukiko0802black@gmail.com>
2021-12-05 07:10:31 +0000

    ログイン周り完成

--- app/controllers/customers/items_controller.rb
+++ app/controllers/customers/items_controller.rb
@@ -1,2 +1,20 @@
 class Customers::ItemsController < ApplicationController
+  def index
+    @items = Item.all
+    @customer = current_customer
+    @courses = Courses.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+    @order_item = OrderItem.new
+    @customer = current_customer
+    @courses = Course.all
+  end
+
+  def move_to_signed_in
+    unless customer_signed_in?
+      redirect_to '/customers/sign_in'
+    end
+  end
 end
--- app/controllers/customers/orders_controller.rb
+++ app/controllers/customers/orders_controller.rb
@@ -1,2 +1,40 @@
 class Customers::OrdersController < ApplicationController
-end
+  def new
+    @order = Order.new
+    @cart_item = CartItem.where(customer_id: current_customer.id)
+    @customer = current_customer
+  end
+
+  def create
+    @cart_items = current_customer.cart_items
+    @order = current_customer.order.new(order_params)
+    if order.save
+      @cart_items.each do |cart_item|
+      order_item = OrderItem.new
+      order_item.item_id = cart_item.item_id
+      order_item.number_of_piaces = cart_item.pieces
+      order_item.save
+    end
+      redirect_to orders_complete_orders_path
+      @cart_items.destroy.all
+    else
+      @order = Order.new(order_params)
+      render.new
+    end
+  end
+
+  def index
+    @orders = current_customer.orders.page(params[:page])
+  end
+
+  def show
+    @order = Order.find(params[:id])
+    @ordered_items = @order.order_items
+  end
+
+  def complete
+  end
+
+  private
+  params.require(:order).permit(:customer_id,:logged_out_on, :total_price, :payment_method, :receiver_name, :shipping_postal_code, :delivery_address, :order_status)
+  endrai
